f87f9011cd8ed607a87f0f6e57118fdecf37219b
Shrikant Soni <shrikantsoni809@gmail.com>
2026-01-09 22:08:58 +0530

    update GTM

--- src/app/layout.js
+++ src/app/layout.js
@@ -81,14 +81,11 @@ export default function RootLayout({ children }) {
         {/* ================= GOOGLE TAG MANAGER ================= */}
         <Script id="gtm-id" strategy="afterInteractive">
           {`
-(function(w,d,s,l,i){w[l]=w[l]||[];
-w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
-var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-j.async=true;
-j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-5HPQH6V5');
+(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TCLQBVST');
 `}
         </Script>
 
@@ -154,13 +151,12 @@ f.parentNode.insertBefore(j,f);
       >
         {/* ================= GTM NOSCRIPT ================= */}
         <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-5HPQH6V5"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          />
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TCLQBVST"
+            height="0" width="0" style={{ display: "none", visibility: "hidden" }}>
+          </iframe>
         </noscript>
+
+
         {/* CALL BUTTON */}
         <a href="tel:+7303771900" className="call-float">
           <img
